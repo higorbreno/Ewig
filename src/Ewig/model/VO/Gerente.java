508f37a146abfc1c184e8f85d674de969fa1c4c5
felpsribeiro <felipemch14@gmail.com>
2020-11-02 20:11:18 -0300

    Adicionado as classes Obra e Gerente

---
+++ src/Ewig/model/VO/Gerente.java
@@ -0,0 +1,70 @@
+package Ewig.model.VO;
+
+public class Gerente {
+	private String nome;
+	private String cpf;
+	private String endereco;
+	private String telefone;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		if(nome.isEmpty()){
+			System.out.println("Valor invalido");
+		}	
+		else {
+			this.nome = nome;
+		}
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		if(cpf.isEmpty()){
+			System.out.println("Valor invalido");
+		}	
+		else {
+			this.cpf = cpf;
+		}
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		if(endereco.isEmpty()){
+			System.out.println("Valor invalido");
+		}	
+		else {
+			this.endereco = endereco;
+		}
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		if(telefone.isEmpty()){
+			System.out.println("Valor invalido");
+		}	
+		else {
+			this.telefone = telefone;
+		}
+	}
+	
+	
+	void cadastrar() {
+		//ainda não implementado
+	}
+	void excluir() {
+		//ainda não implementado
+	}
+	void editar() {
+		//ainda não implementado
+	}
+	void visualizarDados() {
+		//ainda não implementado
+	}
+	
+	
+}
--- src/Ewig/model/VO/Obra.java
+++ src/Ewig/model/VO/Obra.java
@@ -1,5 +1,95 @@
 package Ewig.model.VO;
+import java.util.Calendar;
 
 public class Obra{
+	private String titulo;
+	private String genero;
+	private Calendar ano;
+	private String status;
+	private Autor autor;
+	private String avaliador;
+	
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		if(titulo.isEmpty()){
+			System.out.println("Valor invalido");
+		}	
+		else {
+			this.titulo = titulo;
+		}			
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		if(genero.isEmpty()){
+			System.out.println("Valor invalido");
+		}	
+		else {
+			this.genero = genero;
+		}	
+	}
+	public Calendar getAno() {
+		return ano;
+	}
+	public void setAno(Calendar ano) {
+		this.ano = ano;
+		//falta adcionar regras de encapsulamento
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		if(status.isEmpty()){
+			System.out.println("Valor invalido");
+		}	
+		else {
+			this.status = status;
+		}	
+	}
+	public Autor getAutor() {
+		return autor;
+	}
+	public void setAutor(Autor autor) {
+		this.autor = autor;
+		//falta adcionar regras de encapsulamento
+	}
+	public String getAvaliador() {
+		return avaliador;
+	}
+	public void setAvaliador(String avaliador) {
+		if(avaliador.isEmpty()){
+			System.out.println("Valor invalido");
+		}	
+		else {
+			this.avaliador = avaliador;
+		}	
+	}
+	
+	
+	void cadastrar() {
+		//ainda não implementado
+	}
+	void alterar() {
+		//ainda não implementado
+	}
+	void excluir() {
+		//ainda não implementado
+	}
+	void avaliar() {
+		//ainda não implementado
+	}
+	void buscar() {
+		//ainda não implementado
+	}
+	void visualizarDados() {
+		//ainda não implementado
+	}
+	void visualizarObra() {
+		//ainda não implementado
+	}
 	
 }
