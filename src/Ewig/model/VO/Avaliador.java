ba46f4af382ec4a3590c6f16449791ac8e729fdd
Higor Breno <higorbreno02@gmail.com>
2020-11-04 23:26:56 -0300

    Refatoração do código de encapsulamento das classes Usuario, Autor e Avaliador

--- src/Ewig/model/VO/Avaliador.java
+++ src/Ewig/model/VO/Avaliador.java
@@ -10,67 +10,93 @@ public class Avaliador{
 	public String getNome() {
 		return nome;
 	}
+	
 	public void setNome(String nome) {
-		if(nome.isEmpty()){
-			System.out.println("Valor invalido");
-		}	
-		else {
-			this.nome = nome;
+		if(nome != null) {
+			if(!nome.isEmpty()){
+				this.nome = nome;
+				return;
+			}
+		} else {
+			System.out.println("Nome vazio");
 		}
 	}
+	
 	public String getCpf() {
 		return cpf;
 	}
+	
 	public void setCpf(String cpf) {
-		if(cpf.isEmpty()){
-			System.out.println("Valor invalido");
-		}	
-		else {
-			this.cpf = cpf;
+		if(cpf != null) {
+			if(!cpf.isEmpty() || cpf.length() < 11){
+				this.cpf = cpf;
+				return;
+			}
+		} else {
+			System.out.println("Cpf insuficiente");
 		}
 	}
+	
 	public String getEndereco() {
 		return endereco;
 	}
+	
 	public void setEndereco(String endereco) {
-		if(endereco.isEmpty()){
-			System.out.println("Valor invalido");
-		}	
-		else {
-			this.endereco = endereco;
+		if(endereco != null) {
+			if(!endereco.isEmpty()){
+				this.endereco = endereco;
+				return;
+			}
+		} else {
+			System.out.println("Endereço vazio");
 		}
 	}
+	
 	public String getTelefone() {
 		return telefone;
 	}
+	
 	public void setTelefone(String telefone) {
-		if(telefone.isEmpty()){
-			System.out.println("Valor invalido");
-		}	
-		else {
-			this.telefone = telefone;
+		if(telefone != null) {
+			if(!telefone.isEmpty()){
+				this.telefone = telefone;
+				return;
+			}
+		} else {
+			System.out.println("Telefone vazio");
 		}
 	}
+	
 	public Obra[] getObras() {
 		return obras;
 	}
+	
 	public void setObras(Obra[] obras) {
-		this.obras = obras;
+		if(obras != null){
+			this.obras = obras;
+		} else {
+			System.out.println("Vetor de obras nulo");
+		}	
 	}
 	
 	public Avaliador buscar() {
 		//ainda não implementado
+		Avaliador avaliador = new Avaliador();
+		return avaliador;
 	}
 	
 	public void cadastrar() {
 		//ainda não implementado
 	}
+	
 	public void excluir() {
 		//ainda não implementado
 	}
+	
 	public void editar() {
 		//ainda não implementado
 	}
+	
 	public void visualizarDados() {
 		//ainda não implementado
 	}
--- src/Ewig/model/VO/Usuario.java
+++ src/Ewig/model/VO/Usuario.java
@@ -9,34 +9,49 @@ public class Usuario{
 	public String getLogin() {
 		return login;
 	}
+	
 	public void setLogin(String login) {
-		if(login.isEmpty()){
-			System.out.println("Valor invalido");
-		}	
-		else {
-			this.login = login;
+		if(login != null) {
+			if(!login.isEmpty()){
+				this.login = login;
+				return;
+			}
+		} else {
+			System.out.println("Login vazio");
 		}
 	}
+	
 	public String getSenha() {
 		return senha;
 	}
+	
 	public void setSenha(String senha) {
-		if(senha.isEmpty()){
-			System.out.println("Valor invalido");
-		}	
-		else {
-			this.senha = senha;
+		if(senha != null) {
+			if(!senha.isEmpty()){
+				this.senha = senha;
+				return;
+			}
+		} else {
+			System.out.println("Senha vazio");
 		}
 	}
+	
 	public int getTipoAcesso() {
 		return tipoAcesso;
 	}
+	
 	public void setTipoAcesso(int tipoAcesso) {
-		this.tipoAcesso = tipoAcesso;
+		if(tipoAcesso > 3) {
+			this.tipoAcesso = tipoAcesso;
+		} else {
+			System.out.println("Tipo de Acesso inválido");
+		}
 	}
+	
 	public boolean getPermissaoAcesso() {
 		return permissaoAcesso;
 	}
+	
 	public void setPermissaoAcesso(boolean permissaoAcesso) {
 		this.permissaoAcesso = permissaoAcesso;
 	}
@@ -44,12 +59,15 @@ public class Usuario{
 	public void cadastrar() {
 		//ainda não implementado
 	}
+	
 	public void excluir() {
 		//ainda não implementado
 	}
+	
 	public void editar() {
 		//ainda não implementado
 	}
+	
 	public void autenticar() {
 		//ainda não implementado
 	}
