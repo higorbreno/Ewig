07c6023c9c6d60418b28274d641ecfb78141fbac
felpsribeiro <felipemch14@gmail.com>
2020-12-06 17:29:11 -0300

    MenuController

--- src/Ewig/controller/LoginController.java
+++ src/Ewig/controller/LoginController.java
@@ -1,7 +1,6 @@
 package Ewig.controller;
 
 import Ewig.view.Telas;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
--- src/Ewig/controller/MenuController.java
+++ src/Ewig/controller/MenuController.java
@@ -1,5 +1,47 @@
 package Ewig.controller;
 
+import Ewig.view.Telas;
+
 public class MenuController {
+
+	public void irBuscar() {
+		try {
+			Telas.telaBuscar();//por enquanto, somente passa para a tela do menu principal
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void irCadastrar() {
+		try {
+			Telas.telaCadastrar();//por enquanto, somente passa para a tela do menu principal
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}	
+	
+	public void irRelatorio() {
+		try {
+			Telas.telaRelatorio();//por enquanto, somente passa para a tela do menu principal
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}	
+	
+	public void irDefinirAvaliador() {
+		try {
+			Telas.telaDefinirAvaliador();//por enquanto, somente passa para a tela do menu principal
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void irSair() {
+		try {
+			Telas.telaLogin();//por enquanto, somente passa para a tela do menu principal
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 }
