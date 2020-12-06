0e17b1e0d81fba9726df40a5191f74a3b6425b45
Higor Breno <higorbreno02@gmail.com>
2020-12-06 17:04:23 -0300

    Criação de alguns controllers

--- src/Ewig/controller/LoginController.java
+++ src/Ewig/controller/LoginController.java
@@ -1,11 +1,23 @@
 package Ewig.controller;
 
+import Ewig.view.Telas;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class LoginController {
-	public void autenticar(ActionEvent event) {
-		@FXML private Label erroLogin;
+	@FXML private Label erroLogin;
+	@FXML private TextField campoLogin;
+	@FXML private TextField campoSenha;
+	
+	public void autenticar() {
+		//Verifica se existe o login e se a senha corresponde
+		
+		try {
+			Telas.telaMenu();//por enquanto, somente passa para a tela do menu principal
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
