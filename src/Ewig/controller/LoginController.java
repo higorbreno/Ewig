ec008def5b41654c57146211b4cd6bfadb8c1b9c
Higor Breno <higorbreno02@gmail.com>
2020-12-06 17:53:33 -0300

    Código para mover entre telas de alguns controllers

--- src/Ewig/controller/LoginController.java
+++ src/Ewig/controller/LoginController.java
@@ -1,16 +1,8 @@
 package Ewig.controller;
 
 import Ewig.view.Telas;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 
 public class LoginController {
-	@FXML private Label erroLogin;
-	@FXML private TextField campoLogin;
-	@FXML private TextField campoSenha;
-	
 	public void autenticar() {
 		//Verifica se existe o login e se a senha corresponde
 		
