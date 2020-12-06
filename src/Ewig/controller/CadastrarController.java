6853afb2e1e7a7ecb8f846c4ce7e59ba5a1d05bd
felpsribeiro <felipemch14@gmail.com>
2020-12-06 19:04:42 -0300

    DefinirAvaliadorController

--- src/Ewig/controller/CadastrarController.java
+++ src/Ewig/controller/CadastrarController.java
@@ -1,20 +1,36 @@
 package Ewig.controller;
 
+import Ewig.model.VO.ObraVO;
 import Ewig.view.Telas;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 
 public class CadastrarController {
+	@FXML private TextField campoTitulo;
+	@FXML private TextField campoGenero;
+	@FXML private TextField campoAno;
+	@FXML private ChoiceBox campoAutor;
 	@FXML private Label labelMensagem;
 	
 	public void cadastrarObra() {
+		ObraVO o = new ObraVO();
+		o.setTitulo(campoTitulo.getText());
+		o.setGenero(campoGenero.getText());
+		//o.setAno(Integer.parseInt(campoAno.getText()));
+		//o.setAutor(); recebe o valor de campoAutor
+		
 		try {
-			//chama cadastrar de ObraVO
+			//chama cadastrar() de ObraVO
 			labelMensagem.setText("O cadastro foi realizado com sucesso");
+			//mudar a cor do texto para verde
 			labelMensagem.setVisible(true);
 		} catch (Exception e) {
 			e.printStackTrace();
 			labelMensagem.setText("Não foi possivel realizar o cadastro");
+			//mudar a cor do texto para vermelho
 			labelMensagem.setVisible(true);
 		}
 	}
--- src/Ewig/controller/DefinirAvaliadorController.java
+++ src/Ewig/controller/DefinirAvaliadorController.java
@@ -1,5 +1,53 @@
 package Ewig.controller;
 
-public class DefinirAvaliadorController {
+import Ewig.model.VO.ObraVO;
+import Ewig.view.Telas;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 
+public class DefinirAvaliadorController {
+	@FXML private ChoiceBox escolherObra;
+	@FXML private ChoiceBox escolherAvaliador;
+	@FXML private Label labelAutorObra;
+	@FXML private Label labelAnoObra;
+	@FXML private Label labelAvaliadas;
+	@FXML private Label labelAvaliar;
+	@FXML private Label labelMensagem;
+	
+	public void definirAvaliador() {		
+		try {
+			//recebe a obra
+			//recebe o avaliador
+			//setAvaliador na obraVO
+			labelMensagem.setText("Avaliador definido.");
+			labelMensagem.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+			labelMensagem.setText("Erro en definir Avaliador.");
+			labelMensagem.setVisible(true);
+		}
+	}
+	
+	public void dadosObra() {
+		labelAutorObra.setText("Pegar do BD");
+		labelAutorObra.setVisible(true);
+		labelAnoObra.setText("Pegar do BD");
+		labelAnoObra.setVisible(true);
+	}
+	
+	public void dadosAvaliador() {
+		labelAvaliadas.setText("Pegar do BD");
+		labelAvaliadas.setVisible(true);
+		labelAvaliar.setText("Pegar do BD");
+		labelAvaliar.setVisible(true);
+	}
+	
+	public void irMenu() {
+		try {
+			Telas.telaMenu();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
